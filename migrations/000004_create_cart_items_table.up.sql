82a2a44ef0a161cfff07a53282a636751e42f4c5
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-05-21 16:29:33 +0100

    Add unique constraint for cart_id and furniture_id

--- migrations/000004_create_cart_items_table.up.sql
+++ migrations/000004_create_cart_items_table.up.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS cart_items
     id           bigserial PRIMARY KEY,
     cart_id      bigint    NOT NULL REFERENCES cart (id) ON DELETE cascade,
     furniture_id bigserial NOT NULL REFERENCES furniture (id),
-    quantity     int       NOT NULL
+    quantity     int       NOT NULL,
+    UNIQUE (cart_id, furniture_id)
 );
