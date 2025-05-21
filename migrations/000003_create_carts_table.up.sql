3c3749de9f05361753a6b2f2a1414d582f03bcca
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-05-21 15:58:07 +0100

    Create sql migration files for creating and dropping carts table

---
+++ migrations/000003_create_carts_table.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS carts
+(
+    id          bigserial PRIMARY KEY,
+    identity_id uuid                        NOT NULL,
+    created_at  timestamp(0) with time zone NOT NULL DEFAULT now()
+);
